21907f35e14b24b64643f98d1da2f0940a4473f3
VerneriLahteenoja <verneri.lahteenoja@gmail.com>
2024-02-18 20:10:17 +0200

    anecdote votes and most voted

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -2,14 +2,69 @@ import { useState } from 'react'
 
 const RandomAnecdoteButton = (props) => {
   const handleClick = () => {
+    // Generate a random integer between 0 and n length
     const randomNum = Math.floor(Math.random() * props.anecdotes.length)
     props.setter(randomNum)
   }
-  return(
-  <button onClick={handleClick}>{props.text}</button>
+  return (
+    <button onClick={handleClick}>{props.text}</button>
   )
 }
 
+const VoteAnecdoteButton = (props) => {
+  // Create a copy of votes array
+  const newVotes = [...props.votes]
+  console.log(props.votes)
+  const handleVote = () => {
+    // Increase a vote by 1 when vote button clicked
+    newVotes[props.selected] += 1
+    props.setter(newVotes)  
+  }
+  return (
+    <>
+    <button onClick={handleVote}>{props.text}</button>
+    </>
+  )
+}
+
+const Layout = (props) => {
+  // Create an array of n lenght and fill it with 0 as values
+  const [votes, setVotes] = useState(Array(props.anecdotes.length).fill(0))
+  // Get index of most voted
+  const mostVoted = votes.indexOf(Math.max(...votes))
+  return (
+    <>
+      <VoteAnecdoteButton selected={props.selected} votes={votes} setter={setVotes} text={'vote'}/>
+      <RandomAnecdoteButton selected={props.selected} anecdotes={props.anecdotes} setter={props.selectedSetter} text={'next anecdote'}/>
+      <ShowVotes votes={votes[props.selected]} />
+      <MostVotedAnecdote votes={votes} anecdotes={props.anecdotes} most={mostVoted} />
+    </>
+  )
+}
+
+const ShowVotes = (props) => {
+  return (
+    <div>
+      has {props.votes} votes
+    </div>
+  )
+}
+
+const MostVotedAnecdote = (props) => {
+  return (
+    <>
+      <Header text={'Anecdote with most votes'} />
+      <>{props.anecdotes[props.most]}</>
+      <ShowVotes votes={props.votes[props.most]} />
+    </>  
+  )
+}
+
+const Header = (props) => {
+  return(
+    <h1>{props.text}</h1>
+  )
+}
 
 const App = () => {
   const anecdotes = [
@@ -24,12 +79,13 @@ const App = () => {
   ]
    
   const [selected, setSelected] = useState(0)
-
   return (
     <>
-      {anecdotes[selected]}
+      
       <div>
-      <RandomAnecdoteButton anecdotes={anecdotes} selected={selected} setter={setSelected} text={'next anecdote'}/>
+        <Header text={'Anecdote of the day'} />
+        <div>{anecdotes[selected]}</div>
+        <Layout anecdotes={anecdotes} selected={selected} selectedSetter={setSelected} />
       </div>
     </>
   )
