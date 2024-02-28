caf68474ff03fb3ac411b63b252f009c63c8956d
VerneriLahteenoja <verneri.lahteenoja@gmail.com>
2024-02-28 22:24:05 +0200

    install axios

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,12 +1,30 @@
 import { useState, useEffect } from 'react'
+import axios from 'axios'
 
+const RenderCountries = ({ countries }) => {
+  console.log(countries.map(country => country.name.common))
+  return (
+    <div>
+      {countries.map(country => <div key={country.name.common}>{country.name.common}</div>)}
+    </div>
+  )
+}
 
 function App() {
   const [count, setCount] = useState(0)
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {
+    axios
+    .get("https://studies.cs.helsinki.fi/restcountries/api/all")
+    .then(result => {
+      setCountries(result.data)
+    })
+  }, [])
 
   return (
     <>
-      
+      <RenderCountries countries={countries} />
     </>
   )
 }
